59e06fe2ad44c48627f7b7cd1d75acc136d1ab41
Nadezhda Vlasenko <nadezhda277135@gmail.com>
2018-07-02 22:21:38 +0300

    Added cars, cleaned

--- Lesson5/src/Car.java
+++ Lesson5/src/Car.java
@@ -111,34 +111,85 @@ abstract class CarBuilder{
 class PassatBuilder extends CarBuilder{
 
     @Override
-    void buildMark() {
-        car.setMark("Volkswagen");
-    }
+    void buildMark() {car.setMark("Volkswagen");}
 
     @Override
-    void buildModel() {
-        car.setModel("Passat");
-    }
+    void buildModel() {car.setModel("Passat");}
 
     @Override
-    void buildYear() {
-        car.setYear(2001);
-    }
+    void buildYear() {car.setYear(2001);}
 
     @Override
-    void buildColor() {
-        car.setColor("Black");
-    }
+    void buildColor() {car.setColor("Black");}
 
     @Override
-    void buildPrice() {
-        car.setPrice(30000);
-    }
+    void buildPrice() {car.setPrice(30000);}
 
     @Override
-    void buildNumber() {
-        car.setNumber(3333);
-    }
+    void buildNumber() {car.setNumber(3333);}
+}
+
+class CorollaBuilder extends CarBuilder{
+
+    @Override
+    void buildMark() {car.setMark("Toyota");}
+
+    @Override
+    void buildModel() {car.setModel("Corolla");}
+
+    @Override
+    void buildYear() {car.setYear(2016);}
+
+    @Override
+    void buildColor() {car.setColor("Gray");}
+
+    @Override
+    void buildPrice() {car.setPrice(21000);}
+
+    @Override
+    void buildNumber() {car.setNumber(1111);}
+}
+
+class CamryBuilder extends CarBuilder{
+
+    @Override
+    void buildMark() {car.setMark("Toyota");}
+
+    @Override
+    void buildModel() {car.setModel("Camry");}
+
+    @Override
+    void buildYear() {car.setYear(2004);}
+
+    @Override
+    void buildColor() {car.setColor("Red");}
+
+    @Override
+    void buildPrice() {car.setPrice(41000);}
+
+    @Override
+    void buildNumber() {car.setNumber(2222);}
+}
+
+class NXBuilder extends CarBuilder{
+
+    @Override
+    void buildMark() {car.setMark("Lexus");}
+
+    @Override
+    void buildModel() {car.setModel("NX");}
+
+    @Override
+    void buildYear() {car.setYear(2008);}
+
+    @Override
+    void buildColor() {car.setColor("White");}
+
+    @Override
+    void buildPrice() {car.setPrice(55000);}
+
+    @Override
+    void buildNumber() {car.setNumber(4444);}
 }
 
 class Director{
--- Lesson5/src/Runner.java
+++ Lesson5/src/Runner.java
@@ -9,9 +9,9 @@ public class Runner {
         Cars cars = getCars();
 
         System.out.println("Cписок автомобилей заданной марки");
-        printResult(cars.getCarsByMark("mark1"));
+        printResult(cars.getCarsByMark("Toyota"));
         System.out.println("Cписок автомобилей заданной модели, которые эксплуатируются больше n лет");
-        printResult(cars.getCarsByModelAndYear("model2", 10));
+        printResult(cars.getCarsByModelAndYear("NX", 5));
         System.out.println("Cписок автомобилей заданного года выпуска, цена которых больше указанной");
         printResult(cars.getCarsByYearAndPrice(2001, 1500));
 
@@ -19,24 +19,21 @@ public class Runner {
 
     public static Cars getCars() {
         Director director = new Director();
+        director.setCarBuilder(new CorollaBuilder());
+        Car carB1 = director.buildCar();
+        director.setCarBuilder(new CamryBuilder());
+        Car carB2 = director.buildCar();
         director.setCarBuilder(new PassatBuilder());
         Car carB3 = director.buildCar();
-//        Car carB1 = new CarBuilder().buildMark("Toyota").buildModel("Corolla").buildYear(2016).buildColor("Gray").buildPrice(21000).buildNumber(1111).build();
-//        Car carB2 = new CarBuilder().buildMark("Toyota").buildModel("Camry").buildYear(2016).buildColor("Black").buildPrice(44000).buildNumber(2222).build();
-
-        Car car = new Car(1, "mark1", "model1", 2000, "color1", 1111, 1234);
-        Car car1 = new Car(2, "mark1", "model2", 2000, "color2", 2222, 1235);
-        Car car2 = new Car(3, "mark2", "model2", 2012, "color3", 2222, 1236);
-        Car car3 = new Car(4, "mark2", "model2", 2004, "color4", 3000, 1237);
+        director.setCarBuilder(new NXBuilder());
+        Car carB4 = director.buildCar();
 
         Cars cars = new Cars();
-//        cars.addCar(carB1);
-//        cars.addCar(carB2);
+        cars.addCar(carB1);
+        cars.addCar(carB2);
         cars.addCar(carB3);
-        cars.addCar(car);
-        cars.addCar(car1);
-        cars.addCar(car2);
-        cars.addCar(car3);
+        cars.addCar(carB4);
+
         return cars;
     }
 
